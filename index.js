580abdac59e53220105e19b96636e3362bf5cddc
Megan Dacus <meganjustice0609@gmail.com>
2023-03-16 10:57:11 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin (array) {
+    for (const element of array) {
+        if (element['result']=== 'W') {
+            return element['year']
+        }
+    }
+    return undefined
+}
